145ff37617a33b00c87dd101588ef6a721a76bed
AlecSofiste <alecsofiste@gmail.com>
2025-07-19 14:29:53 -0300

    First Commit

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.janela').forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.transform = 'scale(1.1)';
+    });
+    link.addEventListener('mouseout', () => {
+        link.style.transform = 'scale(1)';
+    });
+});
